6d4f54e60ff4699715c3a1e1819467a97817af50
Ivy Nyambura <ikagam018@gmail.com>
2024-07-25 11:39:28 +0300

    finished

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+ let abba = word.split("").reverse().join("")
+ 
+ 
+  if(word === abba){
+    return true
+  }else
+  return false
+ 
 }
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
   Add written explanation of your solution here
-*/
+*///The function returns true  if the argument is a palindrome and false if it is not. The function is case sensitive
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("abba"))
 
   console.log("");
 
